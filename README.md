68b7a36d5d2d89fda1644fb99bf3ca6c18f15f20
Reeshu <87052176+Reeshuxd@users.noreply.github.com>
2022-02-03 10:07:09 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # AutoApproverBot
-A bot which will auto detect and approves upcoming join requests.
+A telegram to automatically accept upcoming join requests in a chat.
+
+## Info
+`Variables` - `TOKEN`: Get this value from [Botather](https://t.me/botfather).\
+`Rights needed` - Bot will need rights `CanInviteUsers` to accept join requests.\
+`Commands` - No command is needed, just add bot to thw chat and make admin.
+
+## Deploy
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+## Support 
+For any quries, head to our [telegram support](https://t.me/userchatroom) chat!
+You can report bugs there, can request other bots, and don't forget to 🌟 the repo!
---
+++ main.go
@@ -0,0 +1,94 @@
+//   Approver Bot
+//   Copyright (C) 2021 Reeshuxd (@reeshuxd)
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/PaulSonOfLars/gotgbot/v2"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
+)
+
+func main() {
+	bot, err := gotgbot.NewBot(
+		os.Getenv("TOKEN"),
+		&gotgbot.BotOpts{
+			APIURL:      "",
+			Client:      http.Client{},
+			GetTimeout:  gotgbot.DefaultGetTimeout,
+			PostTimeout: gotgbot.DefaultPostTimeout,
+		},
+	)
+	if err != nil {
+		fmt.Println("Failed to create bot:", err.Error())
+	}
+	updater := ext.NewUpdater(
+		&ext.UpdaterOpts{
+			ErrorLog: nil,
+			DispatcherOpts: ext.DispatcherOpts{
+				Error: func(b *gotgbot.Bot, ctx *ext.Context, err error) ext.DispatcherAction {
+					fmt.Println("an error occurred while handling update:", err.Error())
+					return ext.DispatcherActionNoop
+				},
+				Panic:       nil,
+				ErrorLog:    nil,
+				MaxRoutines: 0,
+			},
+		})
+	dp := updater.Dispatcher
+
+	// Commands
+	dp.AddHandler(handlers.NewCommand("start", Start))
+	dp.AddHandler(handlers.NewChatJoinRequest(nil, Approve))
+
+	// Start Polling()
+	poll := updater.StartPolling(bot, &ext.PollingOpts{DropPendingUpdates: true})
+	if poll != nil {
+		fmt.Println("Failed to start bot:", poll.Error())
+	}
+
+	fmt.Printf("@%s has been sucesfully started\n💝Made by @ItsReeshu\n", bot.Username)
+	updater.Idle()
+}
+
+func Start(bot *gotgbot.Bot, ctx *ext.Context) error {
+	if ctx.EffectiveChat.Type != "private" {
+		return nil
+	}
+
+	user := ctx.EffectiveSender.User
+	text := `
+<b>Hello <a href="tg://user?id=%v">%v</a></b>
+I am a bot made for accepting newly coming join requests at the time they comes.
+I am made with <a href="go.dev">golang</a> to give a better performance!
+
+Bot made with 💝 by <a href="t.me/AboutReeshu">Reeshu</a> for you!
+<b>Support Chat:</b> @UserChatRoom
+	`
+	ctx.EffectiveMessage.Reply(
+		bot,
+		fmt.Sprintf(text, user.Id, user.FirstName),
+		&gotgbot.SendMessageOpts{
+			ReplyMarkup: gotgbot.InlineKeyboardMarkup{
+				InlineKeyboard: [][]gotgbot.InlineKeyboardButton{{
+					{Text: "My Source Code", Url: "https://github.com/Reeshuxd/AutoApproverBot"},
+				}},
+			},
+			ParseMode:             "html",
+			DisableWebPagePreview: true,
+		},
+	)
+	return nil
+}
+
+func Approve(bot *gotgbot.Bot, ctx *ext.Context) error {
+	_, err := bot.ApproveChatJoinRequest(ctx.EffectiveChat.Id, ctx.EffectiveSender.User.Id)
+	if err != nil {
+		fmt.Println("Error while approving:", err.Error())
+	}
+	return nil
+}
